e8d592d0d42cc3e992a6d64e4e676adf6ac1cc2f
jose fernando <jose.lazarinos@tecsup.edu.pe>
2021-08-30 22:50:33 -0500

    lab02

---
+++ Lab02-01/frmLogin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02_01
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+            PrincipalMDI principal = new PrincipalMDI();
+            principal.Show();
+            this.Hide();
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
