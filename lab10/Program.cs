572ed611e13ba7da9ae8d399865548a678da6256
jose fernando <jose.lazarinos@tecsup.edu.pe>
2021-11-06 10:39:55 -0500

    lab10

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -10,6 +10,51 @@ namespace lab10
     {
         static void Main(string[] args)
         {
+            NorthwndDataContext context = new NorthwndDataContext();
+
+            // var query = from p in context.Products
+            //where p.Categories.CategoryName == "Beverages"
+            //           where p.UnitPrice < 20
+            //         select p;
+            Products p = new Products();
+            p.ProductName = "Peruvian Coffe";
+            p.SupplierID = 20;
+            p.CategoryID = 1;
+            p.QuantityPerUnit = "10pkgs";
+            p.UnitPrice = 25;
+
+            context.Products.InsertOnSubmit(p);
+            context.SubmitChanges();
+
+
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Price={1} \t Name={2}", prod.ProductID, prod.UnitPrice,prod.ProducName);
+            }
+            Console.ReadKey();
+
+            NorthwndDataContext context = new NorthwndDataContext();
+            var product = (from p in context.Products
+                           where p.ProductName == "Tofu"
+                           select p).FirstOrDefault();
+
+            product.UnitPrice = 100;
+            product.UnitsInStock = 15;
+            product.Discontinued = true;
+
+            context.SubmitChanges();
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Price={1} \t Name={2}", prod.ProductID, prod.UnitPrice, prod.ProducName);
+            }
+            Console.ReadKey();
+
+            NorthwndDataContext context = new NorthwndDataContext();
+            var product = (from p in context.Products
+                           where p.ProductID == 78
+                           select p).FirstOrDefault();
+            context.Products.DeleteOnSubmit(product);
+            context.SubmitChanges();
         }
     }
 }
